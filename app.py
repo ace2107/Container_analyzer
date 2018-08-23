baecbbbbd58783eb37a094036e585970a95a566e
Akash Parekh <aparekh@redhat.com>
2018-08-23 14:33:32 -0400

    moving towards deployment

--- app.py
+++ app.py
@@ -6,31 +6,30 @@ from thoth.common import get_service_account_token
 internal_registry = "https://openshift.default.svc.cluster.local/oapi/v1/namespaces/dh-stage-jupyterhub/imagestreams"
 api_url = "http://user-api-fpokorny-thoth-dev.cloud.paas.upshift.redhat.com/api/v1/analyze"
 
-#namespace input taken from user
+#monitor multiple namespaces
 API_TOKEN = get_service_account_token()
 headers = {'Authorization':'Bearer %s' %API_TOKEN}
 
-response1 = requests.get(internal_registry,headers = headers,verify = False)
-response = response1.text
-response2 = response1.json()
+response = requests.get(internal_registry,headers = headers,verify = False)
+r = response.json()
 
-print(json.dumps(response2, indent = 4,sort_keys = True))
-print(response1.url)
-print(response1.status_code)
+print(json.dumps(r, indent = 4,sort_keys = True))
+print(response.url)
+print(response.status_code)
 
-temp = re.findall(r'"dockerImageRepository":.(.*?)"tags',response)
-images = []
-for item in temp:
-    a = item.replace('"','')
-    b = a.replace("'","")
-    c = b.replace(",","")
-    d = c.replace(" ","")
-    images.append(d)
+containerimages = []
+imageshashes = []
 
-for image in images:
-    print(image)
+for doc in r["items"]:
+        dockerimages.append(doc["status"]["dockerImageRepository"])
+        print(doc["status"]["dockerImageRepository"])
+        for l in doc["status"]["tags"]:
+                for k in l["items"]:
+                        imagekeys.append(k["image"])
+                        print(k["image"])
+        print("\n \n ")
 
-for image in images:
+for image in containerimages:
     PARAMS = (
     ('image', image),
     ('registry_user','container-analyzer-sa'),
@@ -39,9 +38,9 @@ for image in images:
     ('verify_tls','false')
     )
 
-    r = requests.post(url=api_url,params=PARAMS,verify = False)
-    print(r.url)
-    print(r.status_code)
+    resp = requests.post(url=api_url,params=PARAMS,verify = False)
+    print(resp.url)
+    print(resp.status_code)
     analysis_results=r.json()
     print(analysis_results)
     break
