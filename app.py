0fe9d98c0069ea3fcb5115018af70b35191e1cb4
Akash Parekh <aparekh@redhat.com>
2018-08-23 09:52:33 -0400

    changes

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import requests
+import json
+import re
 from thoth.common import get_service_account_token
 
 internal_registry = "https://openshift.default.svc.cluster.local/oapi/v1/namespaces/dh-stage-jupyterhub/imagestreams"
@@ -7,15 +9,13 @@ api_url = "http://user-api-fpokorny-thoth-dev.cloud.paas.upshift.redhat.com/api/
 #namespace input taken from user
 API_TOKEN = get_service_account_token()
 headers = {'Authorization':'Bearer %s' %API_TOKEN}
-print(headers)
 
 response1 = requests.get(internal_registry,headers = headers,verify = False)
+response = response1.text
 response2 = response1.json()
 
 parsed = json.loads(response2)
-print(json.dumps(parsed, indent=4, sort_keys=True))
-
-response = response1.text
+print(json.dumps(parsed, indent = 4,sort_keys = True))
 print(response1.url)
 print(response1.status_code)
 
