0e6ea575cf8c4f2ca83e1f6c63653239f51c45b1
Akash Parekh <aparekh@redhat.com>
2018-08-24 11:58:32 -0400

    Added app-job.py and new app.py

---
+++ app-job.py
@@ -0,0 +1,46 @@
+import requests
+import json
+import re
+from thoth.common import get_service_account_token
+
+internal_registry = "https://openshift.default.svc.cluster.local/oapi/v1/namespaces/dh-stage-jupyterhub/imagestreams"
+api_url = "http://user-api-fpokorny-thoth-dev.cloud.paas.upshift.redhat.com/api/v1/analyze"
+
+#monitor multiple namespaces
+API_TOKEN = get_service_account_token()
+headers = {'Authorization':'Bearer %s' %API_TOKEN}
+
+response = requests.get(internal_registry,headers = headers,verify = False)
+r = response.json()
+
+print(json.dumps(r, indent = 4,sort_keys = True))
+print(response.url)
+print(response.status_code)
+
+containerimages = []
+imageshashes = []
+
+for doc in r["items"]:
+        containerimages.append(doc["status"]["dockerImageRepository"])
+        print(doc["status"]["dockerImageRepository"])
+        for l in doc["status"]["tags"]:
+                for k in l["items"]:
+                        imageshashes.append(k["image"])
+                        print(k["image"])
+        print("\n \n ")
+
+for image in containerimages:
+    PARAMS = (
+    ('image', image),
+    ('registry_user','container-analyzer-sa'),
+    ('registry_password',API_TOKEN),
+    ('debug','true'),
+    ('verify_tls','false')
+    )
+
+    resp = requests.post(url=api_url,params=PARAMS,verify = False)
+    print(resp.url)
+    print(resp.status_code)
+    analysis_results=r.json()
+    print(analysis_results)
+    break
--- app.py
+++ app.py
@@ -1,46 +1,49 @@
+#!/usr/bin/env python3
+#To be continued
+#see app-job.py to run cronjob
+__version__ = '0.0.1'
+__author__ = 'Akash Parekh <aparekh@redhat.com>'
+
+import os
+import sys
+import logging
+from pathlib import Path
+
 import requests
 import json
-import re
-from thoth.common import get_service_account_token
 
-internal_registry = "https://openshift.default.svc.cluster.local/oapi/v1/namespaces/dh-stage-jupyterhub/imagestreams"
-api_url = "http://user-api-fpokorny-thoth-dev.cloud.paas.upshift.redhat.com/api/v1/analyze"
+import daiquiri
+import kubernetes
+import openshift
 
-#monitor multiple namespaces
-API_TOKEN = get_service_account_token()
-headers = {'Authorization':'Bearer %s' %API_TOKEN}
+daiquiri.setup(level=logging.DEBUG if bool(int(os.getenv('DEBUG_CONTAINER-ANALYZER', 0))) else logging.INFO)
 
-response = requests.get(internal_registry,headers = headers,verify = False)
-r = response.json()
+# Load in-cluster configuration that is exposed by OpenShift/k8s configuration.
+kubernetes.config.load_incluster_config()
 
-print(json.dumps(r, indent = 4,sort_keys = True))
-print(response.url)
-print(response.status_code)
+_LOGGER = logging.getLogger('container-analyzer')
+_NAMESPACE = Path('/run/secrets/kubernetes.io/serviceaccount/namespace').read_text()
+_K8S_API = kubernetes.client.CoreV1Api()
+_OCP_BUILD = openshift.client.BuildOpenshiftIoV1Api(openshift.client.ApiClient())
 
-containerimages = []
-imageshashes = []
+"""
+# Payload sent to trigger analysis.
+_PAYLOAD = {
+    "kind": "BuildRequest",
+    "apiVersion": "build.openshift.io/v1",
+    "metadata": {
+        "name": "XXX",
+    },
+    "triggeredBy": [{
+        "message": "DownShift triggered"
+    }],
+    "dockerStrategyOptions": {},
+    "sourceStrategyOptions": {}
+}
+"""
+def main():
+    watcher = kubernetes.watch.Watch()
 
-for doc in r["items"]:
-        containerimages.append(doc["status"]["dockerImageRepository"])
-        print(doc["status"]["dockerImageRepository"])
-        for l in doc["status"]["tags"]:
-                for k in l["items"]:
-                        imageshashes.append(k["image"])
-                        print(k["image"])
-        print("\n \n ")
-
-for image in containerimages:
-    PARAMS = (
-    ('image', image),
-    ('registry_user','container-analyzer-sa'),
-    ('registry_password',API_TOKEN),
-    ('debug','true'),
-    ('verify_tls','false')
-    )
-
-    resp = requests.post(url=api_url,params=PARAMS,verify = False)
-    print(resp.url)
-    print(resp.status_code)
-    analysis_results=r.json()
-    print(analysis_results)
-    break
+if __name__ == '__main__':
+    print("Running Container-analyzer version", __version__)
+    sys.exit(main())
