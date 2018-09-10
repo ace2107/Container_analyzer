cbdfca548ef3b516c8c561e47dac35424a844511
Akash Parekh <aparekh@redhat.com>
2018-09-10 15:47:35 -0400

    Event driven

--- app.py
+++ app.py
@@ -45,8 +45,7 @@ _PAYLOAD = {
 def main():
     watcher = kubernetes.watch.Watch()
     for event in watcher.stream(_K8S_API.list_namespaced_event,namespace=_NAMESPACE):
-        event = json.dumps(event)
-        r = json.loads(event)
+        r = event.json()
         print(json.dumps(r, indent = 4,sort_keys = True))
 
 if __name__ == '__main__':
