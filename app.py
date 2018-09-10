bd0a21612aeecc3d90a7a6b659f6a5dfbfa02e5e
Akash Parekh <aparekh@redhat.com>
2018-09-10 15:53:02 -0400

    Event driven

--- app.py
+++ app.py
@@ -45,8 +45,8 @@ _PAYLOAD = {
 def main():
     watcher = kubernetes.watch.Watch()
     for event in watcher.stream(_K8S_API.list_namespaced_event,namespace=_NAMESPACE):
-        r = event.json()
-        print(json.dumps(r, indent = 4,sort_keys = True))
+        for key,value in event:
+            print(key,"\t",value,"\n")
 
 if __name__ == '__main__':
     print("Running Container-analyzer version", __version__)
