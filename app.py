fc0b5cb52ed0a1fc4b26868630eb0f39c2b1580b
Akash Parekh <aparekh@redhat.com>
2018-09-19 16:35:40 -0400

    Enabling openshift client

--- app.py
+++ app.py
@@ -43,31 +43,20 @@ _PAYLOAD = {
 }
 """
 def main():
-    oapi = openshift.client.OapiApi()
-    #watcher = kubernetes.watch.Watch()
-    project_list = oapi.list_project()
+    watcher = kubernetes.watch.Watch()
     configuration = openshift.client.Configuration()
 
-    api_instance = openshift.client.ImageOpenshiftIoV1Api(openshift.client.ApiClient(configuration))
-    namespace = 'dh-stage-jupyterhub' 
+    api_instance = openshift.client.ImageOpenshiftIoV1Api(openshift.client.ApiClient(configuration)) 
     try:
-        api_response = api_instance.list_namespaced_image_stream(namespace)
+        api_response = api_instance.list_namespaced_image_stream(_NAMESPACE)
         print(api_response)
     except ApiException as e:
         print("Exception when calling ImageOpenshiftIoV1Api->list_namespaced_image_stream: %s\n" % e)
 
-    for project in project_list.items:
-        print(project.metadata.name)
-    
-    api_instance3 = openshift.client.AdmissionregistrationApi()
-    try:
-        api_response3 = api_instance3.get_api_group()
-        print(api_response3)
-    except APIException as e:
-        print("Exception when calling AdmissionregistrationApi->get_api_group: %s\n" % e)
-    #for event in watcher.stream(_K8S_API.list_namespaced_event,namespace=_NAMESPACE):
-        #print(event)
-        #print(type(event))
+    API_INSTANCE = openshift.client.EventsV1beta1Api(openshift.client.ApiClient(configuration))
+    for event in watcher.stream(API_INSTANCE.list_namespaced_image_stream(_NAMESPACE)):
+        print(event)
+        print(type(event))
 
 if __name__ == '__main__':
     print("Running Container-analyzer version", __version__)
