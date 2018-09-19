7232313ef87394c8eae738ba1e8d1f50df7bfbc7
Akash Parekh <aparekh@redhat.com>
2018-09-19 15:42:33 -0400

    Enabling Openshift Client

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ import json
 
 import daiquiri
 import kubernetes
-import openshift
+from openshift import client,config
 
 daiquiri.setup(level=logging.DEBUG if bool(int(os.getenv('DEBUG-CONTAINER-ANALYZER', 0))) else logging.INFO)
 
@@ -43,15 +43,30 @@ _PAYLOAD = {
 }
 """
 def main():
-    watcher = kubernetes.watch.Watch()
-    api_instance =  openshift.client.AdmissionregistrationAPI()
+    config.load_kube_config()
+    oapi = client.OapiApi()
+    #watcher = kubernetes.watch.Watch()
+    project_list = oapi.list_project()
+    configuration = openshift.client.Configuration()
+
+    api_instance = openshift.client.ImageOpenshiftIoV1Api(openshift.client.ApiClient(configuration))
+    namespace = 'dh-stage-jupyterhub' # str | object name and auth scope, such as for teams and projects
+
     try:
-        api_response = api_instance.get_api_group()
-        pprint(api_response)
+        api_response = api_instance.list_namespaced_image_stream(namespace, pretty=pretty, _continue=_continue, field_selector=field_selector, include_uninitialized=include_uninitialized, label_selector=label_selector, limit=limit, resource_version=resource_version, timeout_seconds=timeout_seconds, watch=watch)
+        print(api_response)
+    except ApiException as e:
+        print("Exception when calling ImageOpenshiftIoV1Api->list_namespaced_image_stream: %s\n" % e)
+
+    for project in project_list.items:
+        print(project.metadata.name)
+    
+    api_instance3 = openshift.client.AdmissionregistrationApi()
+    try:
+        api_response3 = api_instance3.get_api_group()
+        print(api_response3)
     except APIException as e:
         print("Exception when calling AdmissionregistrationApi->get_api_group: %s\n" % e)
-        
-
     #for event in watcher.stream(_K8S_API.list_namespaced_event,namespace=_NAMESPACE):
         #print(event)
         #print(type(event))
